0c5826157bf76e7dffe46ec122789c4f8bf94a90
xiexiaowen <dawenxiedawen@gmail.com>
2026-01-20 16:45:19 +0800

    提交示例代码

---
+++ 1714080902309-示例/第一阶段作业/app/src/main/java/com/jx/hzu1714080902309/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jx.hzu1714080902309;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
